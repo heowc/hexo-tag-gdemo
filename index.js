a08945fb78be2931a657258dd6cb6528b5697d9a
Jan-Henrik Damaschke <jdamaschke@outlook.de>
2019-07-06 16:29:18 +0200

    Added highlighting capabilities with prismjs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var Prism = require('../../node_modules/prismjs/prism.js');
+
 const GDEMO_VERSION = '0.8.0';
 const GDEMO_STYLE_LITERAL = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glorious/demo@${GDEMO_VERSION}/dist/gdemo.min.css">`;
 const GDEMO_SCRIPT_LITERAL = `<script src="https://cdn.jsdelivr.net/npm/@glorious/demo@${GDEMO_VERSION}/dist/gdemo.min.js"></script>`;
+const PRISM_STYLE_LITERAL = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs/themes/prism-tomorrow.css">`;
 
 hexo.extend.tag.register('gdemo_terminal', function (args, content) {
-   
+
     if (!args[0]) {
         console.error('command is empty');
         return;
@@ -17,6 +20,9 @@ hexo.extend.tag.register('gdemo_terminal', function (args, content) {
     const onCompleteDelay = args[3] || 0;
     const promptString = args[4] || '$';
     const id = args[5] || 'demo-terminal';
+    const highlightLang = args[6] || 'javascript';
+
+    require(`../../node_modules/prismjs/components/prism-${highlightLang}`);
 
     content = content.replaceAll('\\', '\\\\');
     content = content.replaceAll('\`', '\\`');
@@ -30,7 +36,12 @@ hexo.extend.tag.register('gdemo_terminal', function (args, content) {
 
     for (let i = 0; i < commands.length; i++)
     {
-        demo += `.command(\`${commands[i]}\`, {onCompleteDelay: ${onCompleteDelay}})\n`;
+        const highlightedCode = Prism.highlight(
+            commands[i],
+            Prism.languages[highlightLang],
+            highlightLang
+        );
+        demo += `.command(\`${highlightedCode}\`, {onCompleteDelay: ${onCompleteDelay}})\n`;
     }
 
     demo += `
@@ -40,7 +51,7 @@ hexo.extend.tag.register('gdemo_terminal', function (args, content) {
 
     const script = `<script>${demo}</script>`;
 
-    return `${GDEMO_STYLE_LITERAL}${GDEMO_SCRIPT_LITERAL}
+    return `${PRISM_STYLE_LITERAL}${GDEMO_STYLE_LITERAL}${GDEMO_SCRIPT_LITERAL}
             <div id='${id}' style='height: ${minHeight}'></div>
             ${script}`;
 }, {ends: true});
@@ -56,20 +67,29 @@ hexo.extend.tag.register('gdemo_editor', function (args, content) {
     const windowTitle = args[1] || 'bash';
     const onCompleteDelay = args[2] || 0;
     const id = args[3] || 'demo-editor';
+    const highlightLang = args[4] || 'javascript';
+
+    require(`../../node_modules/prismjs/components/prism-${highlightLang}`);
 
     content = content.replaceAll('\\', '\\\\');
     content = content.replaceAll('\`', '\\`');
 
+    const highlightedCode = Prism.highlight(
+        content,
+        Prism.languages[highlightLang],
+        highlightLang
+    );
+
     const demo = `
         new GDemo('#${id}')
           .openApp('editor', {minHeight: '${minHeight}', windowTitle: '${windowTitle}'})
-          .write(\`${content}\`, {onCompleteDelay: ${onCompleteDelay}})
+          .write(\`${highlightedCode}\`, {onCompleteDelay: ${onCompleteDelay}})
           .end();
     `;
 
     const script = `<script>${demo}</script>`;
 
-    return `${GDEMO_STYLE_LITERAL}${GDEMO_SCRIPT_LITERAL}
+    return `${PRISM_STYLE_LITERAL}${GDEMO_STYLE_LITERAL}${GDEMO_SCRIPT_LITERAL}
             <div id='${id}' style='height: ${minHeight}'></div>
             ${script}`;
 }, {ends: true});
