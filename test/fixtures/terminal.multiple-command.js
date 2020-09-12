ff9b9153fb729212d37d1814c638ff34f23071c8
heowc <heowc1992@gmail.com>
2020-09-12 22:34:17 +0900

    #19 Add test case for results after rendering

--- test/fixtures/terminal.multiple-command.js
+++ test/fixtures/terminal.multiple-command.js
@@ -4,9 +4,9 @@ exports.content = [
     '{% endgdemo_terminal %}',
 ].join('\n');
 
-exports.actual = `<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@glorious/demo@0.11.1/dist/gdemo.min.css">
+exports.actual = `<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/@glorious/demo@0.11.2/dist/gdemo.min.css">
             <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/prismjs/themes/prism-tomorrow.css">
-            <script src="//cdn.jsdelivr.net/npm/@glorious/demo@0.11.1/dist/gdemo.min.js"></script>
+            <script src="//cdn.jsdelivr.net/npm/@glorious/demo@0.11.2/dist/gdemo.min.js"></script>
             <div id='demo-terminal' style='height: 0px'></div>
             <script>
         new GDemo('#demo-terminal')
