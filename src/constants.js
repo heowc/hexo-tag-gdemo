d059698d0e6e1f855f57660519ee2aedca5adcb7
heowc <heowc1992@gmail.com>
2020-12-25 14:42:10 +0900

    Upgrade gdemo version (0.11.2 -> 0.11.4)

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const DEFAULT_GDEMO_VERSION = '0.11.2';
+export const DEFAULT_GDEMO_VERSION = '0.11.4';
 export const DEFAULT_GDEMO_STYLE_URL = `//cdn.jsdelivr.net/npm/@glorious/demo@${DEFAULT_GDEMO_VERSION}/dist/gdemo.min.css`;
 export const DEFAULT_GDEMO_SCRIPT_URL = `//cdn.jsdelivr.net/npm/@glorious/demo@${DEFAULT_GDEMO_VERSION}/dist/gdemo.min.js`;
 export const DEFAULT_PRISMJS_THEME = 'tomorrow';
